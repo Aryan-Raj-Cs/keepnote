43e5bc25166d1fc1791993199eaff7c47a4d47a1
Aryan-Raj-Cs <aryanrajresonance@gmail.com>
2020-07-11 12:11:05 +0530

    Updates

---
+++ precache-manifest.c74550bc04b027ce497ff19b39939f3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa67c07a5b932d3a678a04761493ed4",
+    "url": "/keepnote/index.html"
+  },
+  {
+    "revision": "ceb582fac18f9eba57e5",
+    "url": "/keepnote/static/css/main.bd63101f.chunk.css"
+  },
+  {
+    "revision": "c843e0f11c23e9095762",
+    "url": "/keepnote/static/js/2.8c75fb6e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keepnote/static/js/2.8c75fb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb582fac18f9eba57e5",
+    "url": "/keepnote/static/js/main.a7684bd2.chunk.js"
+  },
+  {
+    "revision": "dfdfc10d9891eebbf2fb",
+    "url": "/keepnote/static/js/runtime-main.65640d83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/keepnote/static/media/logo.5d5d9eef.svg"
+  }
+]);
